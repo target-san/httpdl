24a4e4ff469b75a0504299800a25ff6a5d0de9d9
Target-san <target.san@gmail.com>
2016-11-16 19:06:18 +0200

    Hashmap is now consumed as moving iterator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate clap;
 extern crate hyper;
-extern crate thread_id; // FIXME: drop this one, not needed
 extern crate thread_scoped;
 
 use std::path::Path;
@@ -9,21 +8,34 @@ use std::collections::{HashMap};
 
 use thread_scoped::scoped;
 
-fn pull_files<'a>(dest_folder: &Path, list: &'a Mutex<HashMap<&'a str, Vec<&'a str> >>) {
-    println!("Thread {:?} finished", thread_id::get());
+fn pull_files<'a, I>(dest_folder: &'a Path, list: &'a Mutex<I>)
+    where I: Iterator<Item = (&'a str, Vec<&'a str>)>
+{
+    loop {
+        // Having this as separate expression should prevent locking for the whole duration
+        let item = list.lock().unwrap().next();
+        match item {
+            None => return,
+            Some((url, dests)) => {
+                // TODO: load each acquired URL here
+            }
+        }
+    }
 }
 
 fn main() {
     let dest_folder = Path::new("/home/igor");
     let n_threads = 2;
-    let files_map = Mutex::new(HashMap::new());
+    let files_map: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    let files_seq = Mutex::new(files_map.into_iter().fuse());
     let mut n_guards  = Vec::with_capacity(n_threads - 1);
 
     for _ in 0..n_threads - 1 {
         n_guards.push(
-            unsafe { scoped(|| pull_files(&dest_folder, &files_map) ) }
+            unsafe { scoped(|| pull_files(&dest_folder, &files_seq) ) }
         );
     }
 
-    pull_files(&dest_folder, &files_map);
+    pull_files(&dest_folder, &files_seq);
 }
