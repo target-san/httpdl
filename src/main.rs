30ee7c92c8e9499aa03dc18f6466943ad222b38c
Target-san <target.san@gmail.com>
2016-11-17 00:06:59 +0200

    Changed author No more Vec for thread guards, they're now nested instead Rudimentary args parsing with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+#[macro_use]
 extern crate clap;
 extern crate hyper;
 extern crate thread_scoped;
 
+use std::str::FromStr;
 use std::path::Path;
 use std::sync::{Mutex};
 use std::collections::{HashMap};
 
 use thread_scoped::scoped;
 
-fn pull_files<'a, I>(dest_folder: &'a Path, list: &'a Mutex<I>)
-    where I: Iterator<Item = (&'a str, Vec<&'a str>)>
+fn pull_files<'a, I>(n_children: usize, dest_folder: &'a Path, list: &'a Mutex<I>)
+    where I: Iterator<Item = (&'a str, Vec<&'a str>)> + Send
 {
+    // If child processing threads are required, then construct scoped thread
+    // which would receive all the arguments and children count reduced by 1
+    let child_thread_guard =
+        if n_children == 0 { None }
+        else {
+            Some(unsafe { scoped(|| pull_files(n_children - 1, dest_folder, list)) })
+        };
+
     loop {
         // Having this as separate expression should prevent locking for the whole duration
         let item = list.lock().unwrap().next();
@@ -24,18 +34,35 @@ fn pull_files<'a, I>(dest_folder: &'a Path, list: &'a Mutex<I>)
 }
 
 fn main() {
-    let dest_folder = Path::new("/home/igor");
-    let n_threads = 2;
+    // First, configure our command line
+    let args = clap_app!(httpdl =>
+        (version: crate_version!())
+        (author:  crate_authors!())
+        (about: "Downloads files via HTTP")
+        (@arg directory:   -o +required +takes_value
+            "Directory where to place downloaded files"
+        )
+        (@arg list_file:   -f +required +takes_value
+            "File which contains list of URLs to download and the local file names to use"
+        )
+        (@arg speed_limit: -l +takes_value
+            "Limit download speed to specified value, in bytes; suffixes like 'k' (kilobytes) and 'm' (megabytes)"
+        )
+        (@arg threads_num: -n +takes_value
+            "Download files using N threads"
+        )
+    ).get_matches();
+
+    let dest_dir    = Path::new(args.value_of("directory").unwrap_or("."));
+    let list_file   = args.value_of("list_file").unwrap();
+    let speed_limit = args.value_of("speed_limit")
+        .map(|lim| usize::from_str(lim));
+    let threads_num = 1; /*args.value_of("threads_num")
+        .map(|n| usize::from_str(n)).unwrap_or(1);*/
+
     let files_map: HashMap<&str, Vec<&str>> = HashMap::new();
 
     let files_seq = Mutex::new(files_map.into_iter().fuse());
-    let mut n_guards  = Vec::with_capacity(n_threads - 1);
 
-    for _ in 0..n_threads - 1 {
-        n_guards.push(
-            unsafe { scoped(|| pull_files(&dest_folder, &files_seq) ) }
-        );
-    }
-
-    pull_files(&dest_folder, &files_seq);
+    pull_files(threads_num - 1, dest_dir, &files_seq);
 }
