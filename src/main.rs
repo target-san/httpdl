9dfdb58f0ee6bb92659f4f9e59ca1d451e1f592a
Target-san <target.san@gmail.com>
2016-11-17 23:34:14 +0200

    Nicer error reporting when parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate clap;
 extern crate hyper;
 extern crate thread_scoped;
 
+use std::process::exit;
+use std::fmt::Arguments;
+use std::fs;
+use std::io::{self, Write};
 use std::str::FromStr;
 use std::path::Path;
 use std::sync::{Mutex};
@@ -10,7 +14,7 @@ use std::collections::{HashMap};
 
 use thread_scoped::scoped;
 
-fn pull_files<'a, I>(dest_folder: &'a Path, list: &'a Mutex<I>)
+fn pull_files<'a, I>(dest_folder: &'a str, list: &'a Mutex<I>)
     where I: Iterator<Item = (&'a str, Vec<&'a str>)> + Send
 {
     loop {
@@ -19,6 +23,7 @@ fn pull_files<'a, I>(dest_folder: &'a Path, list: &'a Mutex<I>)
         match item {
             None => return,
             Some((url, dests)) => {
+                let client = hyper::Client::new();
                 // TODO: load each acquired URL here
             }
         }
@@ -44,13 +49,48 @@ fn main() {
             "Download files using N threads"
         )
     ).get_matches();
+    // Complains about error processing specified arg, then dumps usage and exits with error
+    let fail_arg = |name: &str, format: Arguments| -> ! {
+        let _ = writeln!(&mut io::stderr(), "<{}>: {}\n{}", name, format, &args.usage());
+        exit(1)
+    };
 
-    let dest_dir    = Path::new(args.value_of("directory").unwrap_or("."));
+    // Read and validate destination dir
+    // NB: can unwrap because it's required and thus can't be None
+    let dest_dir    = args.value_of("directory").unwrap();
+    {
+        match fs::metadata(dest_dir) {
+            Err(_)   => fail_arg("directory", format_args!("'{}' does not exist or inaccessible", dest_dir)),
+            Ok(meta) => if !meta.is_dir() {
+                fail_arg("directory", format_args!("'{}' is not a directory", dest_dir))
+            }
+        }
+    }
+    // Read and validate path to lists file
+    // NB: can unwrap because it's required and thus can't be None
     let list_file   = args.value_of("list_file").unwrap();
-    let speed_limit = args.value_of("speed_limit")
-        .map(|lim| usize::from_str(lim));
-    let threads_num = 1; /*args.value_of("threads_num")
-        .map(|n| usize::from_str(n)).unwrap_or(1);*/
+    {
+        match fs::metadata(list_file) {
+            Err(_)   => fail_arg("list_file", format_args!("'{}' does not exist or inaccessible", list_file)),
+            Ok(meta) => if !meta.is_file() {
+                fail_arg("list_file", format_args!("'{}' is not a file", list_file))
+            }
+        }
+    }
+    // Read and parse download speed limit
+    let speed_limit = match args.value_of("speed_limit") {
+        None => 0,
+        Some(value) => 0 // TODO: proper parsing
+    };
+    // Read and parse number of threads which should be used
+    let threads_num = match args.value_of("threads_num") {
+        None => 1usize,
+        Some(value) => match usize::from_str(value) {
+            Err(_)  => fail_arg("threads_num", format_args!("'{}' cannot be parsed as unsigned integer", value)),
+            Ok(0)   => fail_arg("threads_num", format_args!("cannot be zero")),
+            Ok(num) => num,
+        }
+    };
 
     let files_map: HashMap<&str, Vec<&str>> = HashMap::new();
     // TODO: fill files map
