8a8b02559929004d001121c8b68e7e0e3381c8d4
Target-san <target.san@gmail.com>
2016-11-22 23:42:08 +0200

    Speed limiter based on token bucket algorithm

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ extern crate env_logger;
 extern crate hyper;
 extern crate thread_scoped;
 
+use std::cmp;
 use std::error::Error;
-use std::path::Path;
-use std::process::exit;
 use std::fmt::Arguments;
 use std::fs;
 use std::io::{self, Read, Write};
+use std::path::Path;
+use std::process::exit;
 use std::str::FromStr;
-use std::sync::{Mutex};
+use std::sync::Mutex;
+use std::thread;
+use std::time::Instant;
 
 use hyper::status::StatusCode;
 
@@ -43,17 +46,38 @@ quick_error! {
     }
 }
 
-fn pull_file(src_url: &str, dest_path: &Path) -> Result<(), DownloadError> {
+fn pull_file(src_url: &str, dest_path: &Path, bucket: &Mutex<TokenBucket>) -> Result<(), DownloadError> {
     let mut response = hyper::Client::new().get(src_url).send()?;
     if response.status != hyper::status::StatusCode::Ok {
         return Err(DownloadError::Server(response.status));
     }
     let mut dest_file = fs::File::create(&dest_path)?;
-    let _ = io::copy(&mut response, &mut dest_file)?;
+    let _ = copy_limited(&mut response, &mut dest_file, bucket)?;
     Ok(())
 }
 
-fn pull_files<'a, I>(thread_num: usize, dest_dir: &'a str, list: &'a Mutex<I>)
+fn copy_limited<R: Read + ?Sized, W: Write + ?Sized>(reader: &mut R, writer: &mut W, bucket: &Mutex<TokenBucket>) -> io::Result<u64> {
+    let mut buf = [0; 8 * 1024];
+    let mut written = 0;
+    loop {
+        let limit = bucket.lock().unwrap().take(buf.len());
+        if limit == 0 {
+            thread::yield_now();
+            continue;
+        }
+        let mut part = &mut buf[..limit];
+        let len = match reader.read(&mut part) {
+            Ok(0) => return Ok(written),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&mut part[..len])?;
+        written += len as u64;
+    }
+}
+
+fn pull_files<'a, I>(thread_num: usize, dest_dir: &'a str, bucket: &Mutex<TokenBucket>, list: &'a Mutex<I>)
     where I: Iterator<Item = (&'a str, &'a str)> + Send
 {
     debug!("worker thread #{} started", thread_num);
@@ -64,11 +88,51 @@ fn pull_files<'a, I>(thread_num: usize, dest_dir: &'a str, list: &'a Mutex<I>)
             Some((url, dest)) => (url, Path::new(dest_dir).join(dest)) 
         };
         info!("Thread #{}: Downloading {} -> {}", thread_num, url, dest_path.display());
-        if let Err(error) = pull_file(url, &dest_path) {
+        if let Err(error) = pull_file(url, &dest_path, bucket) {
             error!("#{} {} -> {} failed: {}", thread_num, url, dest_path.display(), error);
         }
     }
     debug!("worker thread #{} finished", thread_num);
+}
+
+struct TokenBucket {
+    fill_rate: usize,
+    capacity:  usize,
+    remaining: f64,
+    timestamp: Instant,
+}
+
+impl TokenBucket {
+    fn new(rate: usize) -> TokenBucket {
+        TokenBucket::with_capacity(rate, rate)
+    }
+
+    fn with_capacity(rate: usize, capacity: usize) -> TokenBucket {
+        TokenBucket {
+            fill_rate: rate,
+            capacity:  capacity,
+            remaining: 0f64,
+            timestamp: Instant::now(),
+        }
+    }
+
+    fn take(&mut self, amount: usize) -> usize {
+        // 0. For zero fillrate, treat this bucket as infinite
+        if self.fill_rate == 0 {
+            return amount;
+        }
+        // 1. Add to bucket rate / delta
+        let now = Instant::now();
+        let delta = now - self.timestamp;
+        self.timestamp = now;
+        let delta_secs = ((delta.as_secs() as f64) + (delta.subsec_nanos() as f64) / 1_000_000_000f64);
+        let delta_fill = delta_secs * (self.fill_rate as f64);
+        self.remaining = (self.remaining + delta_fill).min(self.capacity as f64);
+        // 2. Take as much as possible from bucket, but no more than is present there
+        let taken = cmp::min(self.remaining.floor() as usize, amount);
+        self.remaining = (self.remaining - (taken as f64)).max(0f64);
+        return taken;
+    }
 }
 
 fn main() {
@@ -123,7 +187,7 @@ fn main() {
     }
     // Read and parse download speed limit
     let speed_limit = match args.value_of("speed_limit") {
-        None => 0,
+        None => 100 * 1024,
         Some(_) => fail_arg("speed_limit", format_args!("not supported for now, sorry"))
     };
     // Read and parse number of threads which should be used
@@ -161,16 +225,20 @@ fn main() {
     // Finally, when we just need to consume all this in random order,
     // transform it into consuming iterator and pack under mutex
     let files_seq = Mutex::new(files_map.into_iter().fuse());
+    // Construct locked token bucket with specified limit
+    let bucket = Mutex::new(TokenBucket::new(speed_limit));
     // Now, create N - 1 worker threads and each will pull files
     // Looks simpler than fancy tricks like recursive guards
     let mut worker_guards = Vec::with_capacity(threads_num);
+    // Finally, create worker threads
     for i in 1..threads_num {
         let seq_ref = &files_seq; // thus we can move reference to seq into closure
+        let bucket_ref = &bucket;
         worker_guards.push(
-            unsafe { scoped(move || pull_files(i, dest_dir, seq_ref)) }
+            unsafe { scoped(move || pull_files(i, dest_dir, bucket_ref, seq_ref)) }
         );
     }
     // Main thread would do just the same as worker ones, summing up to N threads
-    pull_files(0, dest_dir, &files_seq);
+    pull_files(0, dest_dir, &bucket, &files_seq);
     // Vector of guards will stop right here
 }
