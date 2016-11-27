7cb943a04250fe31f55632866399bfac3b136a71
Target-san <target.san@gmail.com>
2016-11-27 22:12:15 +0200

    WIP moving arguments parsing to separate function

--- src/main.rs
+++ src/main.rs
@@ -137,69 +137,8 @@ impl TokenBucket {
 
 fn main() {
     let _ = env_logger::init().unwrap();
-    // First, configure our command line
-    let args = clap_app!(httpdl =>
-        (version: crate_version!())
-        (author:  crate_authors!())
-        (about: "Downloads files via HTTP")
-        (@arg directory:   -o +required +takes_value
-            "Directory where to place downloaded files"
-        )
-        (@arg list_file:   -f +required +takes_value
-            "File which contains list of URLs to download and the local file names to use"
-        )
-        (@arg speed_limit: -l +takes_value
-            "Limit download speed to specified value, in bytes; suffixes like 'k' (kilobytes) and 'm' (megabytes)"
-        )
-        (@arg threads_num: -n +takes_value
-            "Download files using N threads"
-        )
-    ).get_matches();
-    // Complains about error processing specified arg, then dumps usage and exits with error
-    let fail_arg = |name: &str, format: Arguments| -> ! {
-        let _ = writeln!(&mut io::stderr(), "<{}>: {}\n{}", name, format, &args.usage());
-        exit(1)
-    };
 
-    // Read and validate destination dir
-    // NB: can unwrap because it's required and thus can't be None
-    let dest_dir    = args.value_of("directory").unwrap();
-    {
-        let meta = match fs::metadata(dest_dir) {
-            Ok(val) => val,
-            Err(_) => fail_arg("directory", format_args!("'{}' does not exist or inaccessible", dest_dir))
-        };
-        if !meta.is_dir() {
-            fail_arg("directory", format_args!("'{}' is not a directory", dest_dir))
-        }
-    }
-    // Read and validate path to lists file
-    // NB: can unwrap because it's required and thus can't be None
-    let list_file   = args.value_of("list_file").unwrap();
-    {
-        let meta = match fs::metadata(list_file) {
-            Ok(val) => val,
-            Err(_) => fail_arg("list_file", format_args!("'{}' does not exist or inaccessible", list_file))
-        };
-        if !meta.is_file() {
-                fail_arg("list_file", format_args!("'{}' is not a file", list_file))
-        }
-    }
-    // Read and parse download speed limit
-    let speed_limit = match args.value_of("speed_limit") {
-        None => 100 * 1024,
-        Some(_) => fail_arg("speed_limit", format_args!("not supported for now, sorry"))
-    };
-    // Read and parse number of threads which should be used
-    let threads_num = match args.value_of("threads_num") {
-        None => 1usize,
-        Some(value) => match usize::from_str(value) {
-            Err(_)      => fail_arg("threads_num", format_args!("'{}' cannot be parsed as unsigned integer", value)),
-            Ok(0usize)  => fail_arg("threads_num", format_args!("cannot be zero")),
-            Ok(num)     => num 
-        }
-    };
-
+    let Args { dest_dir, list_file, thread_num, speed_limit } = parse_args();
     // Now, we read whole list file and then fill files mapping
     let all_text = {
         let mut fd = match fs::File::open(list_file) {
@@ -241,4 +180,85 @@ fn main() {
     // Main thread would do just the same as worker ones, summing up to N threads
     pull_files(0, dest_dir, &bucket, &files_seq);
     // Vector of guards will stop right here
+}
+
+struct Args
+{
+    dest_dir:    String,
+    list_file:   String,
+    thread_num:  usize,
+    speed_limit: usize,
+}
+
+quick_error! {
+    #[derive(Debug)]
+    enum ArgError {
+        Arg(name: &str, error: Box<Error>) {
+            cause(error)
+            display("<{}>: {}", name, error)
+        }
+    }
+}
+
+fn parse_args() -> Args {
+    // First, configure our command line
+    let args = clap_app!(httpdl =>
+        (version: crate_version!())
+        (author:  crate_authors!())
+        (about: "Downloads files via HTTP")
+        (@arg directory:   -o +required +takes_value
+            "Directory where to place downloaded files"
+        )
+        (@arg list_file:   -f +required +takes_value
+            "File which contains list of URLs to download and the local file names to use"
+        )
+        (@arg speed_limit: -l +takes_value
+            "Limit download speed to specified value, in bytes; suffixes like 'k' (kilobytes) and 'm' (megabytes)"
+        )
+        (@arg threads_num: -n +takes_value
+            "Download files using N threads"
+        )
+    ).get_matches();
+
+    let process_args = |args: &clap::ArgMatches| -> Result<Args, Box<Error>>{
+        // Destination directory
+        let dest_dir = args.value_of("directory").unwrap();
+        if ! fs::metadata(dest_dir)?.is_dir() {
+            return Err(format_args!("'{}' is not a directory", dest_dir).into());
+        }
+        // Source list file
+        let list_file = args.value_of("list_file").unwrap();
+        if ! fs::metadata(list_file)?.is_file() {
+            return Err(format_args!("'{}' is not a file", dest_dir).into());
+        }
+        // Number of threads
+        let threads_num = match args.value_of("threads_num") {
+            None => 1usize,
+            Some(value) => match usize::from_str(value) {
+                Err(_)      => fail_arg("threads_num", format_args!("'{}' cannot be parsed as unsigned integer", value)),
+                Ok(0usize)  => fail_arg("threads_num", format_args!("cannot be zero")),
+                Ok(num)     => num 
+            }
+        };
+
+        // Read and parse download speed limit
+        let speed_limit = match args.value_of("speed_limit") {
+            None => 0,
+            Some(_) => fail_arg("speed_limit", format_args!("not supported for now, sorry"))
+        };
+
+        Ok(Args {
+            dest_dir:    dest_dir,
+            list_file:   list_file,
+            threads_num: threads_num,
+            speed_limit: speed_limit,
+        })
+    };
+
+    match process_args(&args) {
+        Ok(value) => value,
+        Err(err)  => {
+
+        }
+    }
 }
