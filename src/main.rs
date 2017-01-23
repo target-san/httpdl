f37cba55769252cc801a498856a6ab26337de1d6
Target-san <target.san@gmail.com>
2017-01-23 14:44:09 +0200

    Read list of files to download

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,38 @@ extern crate clap;      // Declare external crate dependency
 // Import symbols from STDLIB or other crates
 use std::str::FromStr;  // Describes any type which can be read from string slice
 use std::fmt::Debug;    // Debug types can be dumped as text, for debug purposes
+use std::fs;            // Filesystem stuff
+use std::io::Read;      // We need this to invoke methods of Read trait
 // Main entry point
 fn main() {
-    println!("Arguments:\n{:?}", parse_args());
+    // Parse arguments 
+    let args = parse_args();
+    // Read text of list file into single string
+    let file_list_text = {
+        let mut file = fs::File::open(args.list_file).unwrap(); // Open file
+        let mut text = String::new();                           // Create buffer for file text
+        file.read_to_string(&mut text).unwrap();                // Read file text into buffer
+        text                                                    // Return buffer from block
+    };
+    // Construct iterator over list of URLs and file names
+    let urls_list = file_list_text
+        .lines()    // Iterate over lines in buffer
+        .filter_map(|line| { // Maps each value to optional value. Absent values are filtered out
+            // Split each line by whitespace chars, then filter out all empty pieces
+            let mut pieces = line.split(|c| " \r\n\t".contains(c)).filter(|s| !s.is_empty());
+            let url = pieces.next();        // Pick first piece as URL
+            let filename = pieces.next();   // Pick second piece as filename
+            // Next, if both 'url' and 'filename' contain actual values, pack them into a tuple and return
+            if let (Some(url_value), Some(filename_value)) = (url, filename) {
+                Some((url_value, filename_value))
+            }
+            else { None } // Otherwise, signal that there's no pair for this particular line
+        })
+        .fuse();
+    // Just for our convenience, 
+    for item in urls_list {
+        println!("{:?}", item);
+    }
 }
 // Contains parsed arguments
 #[derive(Debug)]
