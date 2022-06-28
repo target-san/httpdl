0bf6b8f4ae31c1577be689c2adc560387e107d19
Target-san <target.san@gmail.com>
2022-06-28 19:55:53 +0300

    WIP: Switching to async tokio-based execution runtime

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,42 @@
 //
 // Uses from stdlib
 //
-use std::cell::RefCell;
-use std::fs;
-use std::io::{Read, Write};
 use std::path::Path;
-use std::sync::Mutex;
 //
 // Uses from external crates
 //
-use clap::Parser;
-use crossbeam_utils::thread::scope;
 use anyhow::Result;
+use clap::Parser;
+use tokio::fs;
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
+use tokio::spawn;
+use reqwest::Client;
 //
 // Submodules
 //
 mod token_bucket;
-use token_bucket::TokenBucket;
 
 mod config;
 use config::Config;
 
-mod copy_with_speedlimit;
-use copy_with_speedlimit::copy_with_speedlimit;
-
 // Program starting point, as usual
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     // First, parse arguments
     let Config { dest_dir, list_file, threads_num, speed_limit } = Config::try_parse()?;
     // Now, we read whole list file and then fill files mapping
     let all_text = {
         // Open file with list of files to download
-        let mut fd = fs::File::open(&list_file)?;
+        let mut fd = fs::File::open(&list_file).await?;
         // Then read all of its contents into buffer
         let mut text = String::new();
-        fd.read_to_string(&mut text)?;
+        fd.read_to_string(&mut text).await?;
         text
     };
     // Next, we split the whole file into lines in-place
     // And for each line which contains proper url-filename tuple,
     // We yield that tuple
-    let mut files_seq = all_text
+    let files_seq = all_text
         .lines()
         .filter_map(|line| {
             let mut pieces = line.split(|c| " \r\n\t".contains(c)).filter(|s| !s.is_empty());
@@ -53,108 +49,60 @@ fn main() -> Result<()> {
         })
         .fuse();
 
-    let mut bucket = TokenBucket::new(speed_limit);
     let dest_dir = Path::new(&dest_dir);
+    let client = Client::new();
 
-    let _ = copy_streams(
-        threads_num,
-        move |_| {
-            files_seq.next().map(|(url, name)| {
-                let dest_path = dest_dir.join(name);
-                let response = reqwest::blocking::get(url)?;
-                let dest_file = fs::File::create(&dest_path)?;
-                
-                Ok((response, dest_file))
-            })
-        },
-        move |amount| bucket.take(amount)
-    );
+    for (i, (url_str, dest_file_str)) in files_seq.enumerate() {
+        let src_url = url::Url::parse(url_str)?;
+        let dest_path = dest_dir.join(dest_file_str);
+        let client = client.clone();
+        let i = i;  // Dupe loop counter into local scope
+
+        spawn(async move {
+            println!(
+                "#{}: Started {} -> {}",
+                i,
+                src_url,
+                dest_path.file_name().and_then(|name| name.to_str()).unwrap_or("")
+            );
+            match download_and_write(client, src_url.clone(), &dest_path).await {
+                Ok(_) => println!(
+                    "#{}: Completed {} -> {}",
+                    i,
+                    src_url,
+                    dest_path.file_name().and_then(|name| name.to_str()).unwrap_or("")
+                ),
+                Err(err) => eprintln!(
+                    "#{}: Failed {} -> {}: {}",
+                    i,
+                    src_url,
+                    dest_path.file_name().and_then(|name| name.to_str()).unwrap_or(""),
+                    err
+                ),
+            }
+        });
+    }
 
     Ok(())
 }
 
-fn copy_streams<F, L, R, W>(threads_num: usize, stream_fn: F, limiter_fn: L) -> Result<()>
-    where
-        F: FnMut(usize) -> Option<Result<(R, W)>>,
-        F: Send,
-        L: FnMut(usize) -> usize,
-        L: Send,
-        R: Read,
-        W: Write
-{
-    assert!(threads_num > 0);
+async fn download_and_write(
+    client: Client,
+    src_url: impl reqwest::IntoUrl,
+    dest_path: impl AsRef<Path>
+) -> Result<()> { 
+    let mut src_body = client.get(src_url).send().await?;
+    let dest_file = fs::File::create(dest_path).await?;
+    let mut dest_file = BufWriter::new(dest_file);
 
-    if threads_num == 1 {
-        let stream_fn = RefCell::new(stream_fn);
-        let stream_fn = move |thread_id| {
-            stream_fn
-                .try_borrow_mut()
-                .ok()
-                .map(|mut inner| inner(thread_id))
-                .flatten()
-        };
-
-        let limiter_fn = RefCell::new(limiter_fn);
-        let limiter_fn = move |amount| {
-            limiter_fn
-                .try_borrow_mut()
-                .ok()
-                .map(|mut inner| inner(amount))
-                .unwrap_or(0)
-        };
-
-        copy_streams_thread(0, &stream_fn, &limiter_fn);
-
-        return Ok(());
+    // Do an asynchronous, buffered copy of the download to the output file
+    while let Some(chunk) = src_body.chunk().await? {
+        dest_file.write(&chunk).await?;
     }
+    
+    // Must flush tokio::io::BufWriter manually.
+    // It will *not* flush itself automatically when dropped.
+    dest_file.flush().await?;
 
-    let stream_fn = Mutex::new(stream_fn);
-    let stream_fn = move |thread_id| {
-        stream_fn
-            .try_lock()
-            .ok()
-            .map(|mut inner| inner(thread_id))
-            .flatten()
-    };
-
-    let limiter_fn = Mutex::new(limiter_fn);
-    let limiter_fn = move |amount| {
-        limiter_fn
-            .try_lock()
-            .ok()
-            .map(|mut inner| inner(amount))
-            .unwrap_or(0)
-    };
-
-    let _ = scope(|s| {
-        for i in 1..threads_num {
-            // Create separate references, so that we can move them to scoped thread
-            let thread_id = i;
-            let stream_fn_ref = &stream_fn;
-            let limiter_fn_ref = &limiter_fn;
-
-            s.spawn(move |_| copy_streams_thread(thread_id, stream_fn_ref, limiter_fn_ref));
-        }
-
-        copy_streams_thread(0, &stream_fn, &limiter_fn);
-    });
-
-    return Ok(());
-}
-
-fn copy_streams_thread<R: Read, W: Write>(
-    thread_id:  usize,
-    stream_fn:  &impl Fn(usize) -> Option<Result<(R, W)>>,
-    limiter_fn: &impl Fn(usize) -> usize
-) {
-    loop {
-        // Retrieve next file in sequence
-        let (mut reader, mut writer) = match stream_fn(thread_id) {
-            None             => break,    // No additional jobs
-            Some(Err(_))     => continue, // Failed, maybe need to do additional reporting
-            Some(Ok((r, w))) => (r, w) 
-        };
-
-        let _ = copy_with_speedlimit(&mut reader, &mut writer, &limiter_fn);
-    }
+    Ok(())
 }
