4feb1379e57da8cb79665d4febd7113a026b2b99
Target-san <target.san@gmail.com>
2022-06-29 14:49:58 +0300

    Added some succeeding async tests of copy_with_speedlimit

--- src/copy_with_speedlimit.rs
+++ src/copy_with_speedlimit.rs
@@ -2,6 +2,8 @@ use std::io::{Result, ErrorKind};
 
 use tokio::task::yield_now;
 use tokio::io::{AsyncRead, AsyncWrite, AsyncReadExt, AsyncWriteExt};
+
+const BUFFER_SIZE: usize = 8 * 1_024;
 /// Performs asynchronous copying from one byte stream into another, with respect to specified speed limiter
 /// 
 /// # Arguments
@@ -25,7 +27,7 @@ pub async fn copy_with_speedlimit<R, W, L>(
         W: AsyncWrite + Unpin + ?Sized,
         L: Fn(usize) -> usize
 {
-    let mut buf = [0u8; 8 * 1_024];
+    let mut buf = [0u8; BUFFER_SIZE];
     let mut written = 0u64;
     loop {
         let limit = limiter(buf.len()).min(buf.len());
@@ -47,12 +49,11 @@ pub async fn copy_with_speedlimit<R, W, L>(
 
 #[cfg(test)]
 mod tests {
-    //! TODO: consider fuzzing Read and Write to write some failing tests
-
     use assert_matches::assert_matches;
     use rand::{thread_rng, Rng, RngCore};
     use super::copy_with_speedlimit;
-    use tokio_test::block_on;
+    use tokio_test::{ block_on, io };
+    use super::BUFFER_SIZE;
 
     fn unlimited(amount: usize) -> usize{
         amount
@@ -64,5 +65,35 @@ mod tests {
 
     fn random_limit(amount: usize) -> usize {
         thread_rng().gen_range(0..=amount)
+    }
+
+    #[test]
+    fn successful_copies() {
+        // Limiter functions
+        let limiters = [
+            unlimited, simple_limit_16, random_limit
+        ];
+        // Sample buffers 
+        let samples: Vec<_> = [0, 10, BUFFER_SIZE - 1, BUFFER_SIZE, BUFFER_SIZE + 1, BUFFER_SIZE * 2]
+            .map(|size| {
+                let mut buf = vec![0u8; size];
+                thread_rng().fill_bytes(&mut buf);
+                buf
+            })
+            .into();
+
+        block_on(async move {
+            for limiter in limiters {
+                for sample in &samples {
+                    let mut reader = io::Builder::new().read(sample).build();
+                    let mut writer = io::Builder::new().write(sample).build();
+
+                    assert_matches!(
+                        copy_with_speedlimit(&mut reader, &mut writer, &limiter).await,
+                        Ok(len) if len == sample.len() as u64
+                    );
+                }
+            }
+        });
     }
 }
